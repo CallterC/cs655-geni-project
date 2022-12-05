bad65e9792ce69c60bf5da59693ac4a202b78cca
callter <quansenw@bu.edu>
2022-12-05 07:09:06 -0500

    quansen - init file creation for server and client

---
+++ md5CrackerClient.py
@@ -0,0 +1,12 @@
+import hashlib
+
+
+def generateMD5(inputString):
+    m = hashlib.md5()
+    m.update
+    return m.hexdigest()
+
+
+if __init__ == "__main__":
+    print("Starting MD5 Cracker")
+    
