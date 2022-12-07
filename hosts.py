f8150eb2da8becf546474c5b1b7fb623aea5491f
KevinLu <kevin@asleague.org>
2022-12-07 14:15:53 -0500

    Update hosts.py

--- hosts.py
+++ hosts.py
@@ -1,3 +1,12 @@
+'''
+Author: Jinqi Lu
+Prof. Matta
+CS 655 PA3 GENI-MINILAB
+Boston University
+December 8, 2022
+python management.py
+This is the config file contains all available hosts and their port
+'''
 hosts = [
     ["127.0.0.1", 9102],
     ["127.0.0.1", 9101],
